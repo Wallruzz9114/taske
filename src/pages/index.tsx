8bf7324670291a644b4203f17552efc8f1dc95bf
Wallruzz9114 <pintojose.benedicto2@gmail.com>
2020-11-16 17:20:01 -0700

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <div className="">
+    <Head>
+      <title>Create nEXT aPP</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className="p-2">
+      <h1 className="font-bold">Welcome to Next.js!</h1>
+    </main>
+  </div>
+);
+
+export default Home;
